f6a7621a9c34d0d15e2bbb03d22ca74f684a56fa
Gustavoalessandro021 <gustavoalessan021@gmail.com>
2025-11-12 11:18:12 -0300

    Remove commented-out SQL commands in banc.py

--- banc.py
+++ banc.py
@@ -6,9 +6,9 @@ banco_alunos = sqlite3.connect('banco_alunos')
 cursor = banco_alunos.cursor()
 
 # cursor.execute("CREATE TABLE alunos(id INTEGER PRIMARY KEY AUTOINCREMENT, nome text, idade integer,email text )")
-# cursor.execute("INSERT INTO alunos(id,nome,idade,email)VALUES(1, 'Juan',17,'juanpedro@gmail.com')") 
-# cursor.execute("INSERT INTO alunos(id,nome,idade,email)VALUES(2, 'Arthur',18,'arthurfreitas@gmail.com')") 
-# cursor.execute("INSERT INTO alunos(id,nome,idade,email)VALUES(3, 'Gustavo',19,'gustavosilva@gmail.com')") 
+# cursor.execute("INSERT INTO alunos(nome,idade,email)VALUES(1, 'Juan',17,'juanpedro@gmail.com')") 
+# cursor.execute("INSERT INTO alunos(nome,idade,email)VALUES(2, 'Arthur',18,'arthurfreitas@gmail.com')") 
+# cursor.execute("INSERT INTO alunos(nome,idade,email)VALUES(3, 'Gustavo',19,'gustavosilva@gmail.com')") 
 
 
 # cursor.execute("SELECT * FROM alunos")
@@ -22,6 +22,7 @@ cursor = banco_alunos.cursor()
 # cursor.execute("DELETE FROM alunos WHERE id = 3 ")
 
 banco_alunos.commit()
+
 
 
 
